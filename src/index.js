c858aead0e62a6477ffd3ed219a6221903795b4d
Maro7772 <mm9922522@gmail.com>
2025-09-03 00:39:46 +0300

    Fixing

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://event-x-front-54ar5wmge-maro7772s-projects.vercel.app",
+      "https://event-x-front-end-git-main-maro7772s-projects.vercel.app",
     ],
     credentials: true,
   })
